4e2b4ddc182ae1a64d763be0e3900314f6ec6fe3
bello3102 <kjh2whw4wn@privaterelay.appleid.com>
2026-06-05 11:55:54 +0000

    Update personal information program

--- Hello.java
+++ Hello.java
@@ -1,8 +1,28 @@
    public class Hello {
          public static void main(String[] args) {
-          System.out.println("Hello World");
+          
+       String fullName = "Kwabena Tete Larbi";
+       int age = 20;
+       char gender = 'M';
+       double height = 1.75;
+       double weight = 68.5;
+       boolean isStudent = true;
+       long phoneNumber = 557817665L;
+       String gradeLevel = "Level 200";
+       String nationality = "Ghanaian";
 
-} 
+   System.out.println("Full Name: " + fullName);
+        System.out.println("Age: " + age);
+        System.out.println("Gender: " + gender);
+        System.out.println("Height: " + height);
+        System.out.println("Weight: " + weight);
+        System.out.println("Student Status: " + isStudent);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Grade Level: " + gradeLevel);
+        System.out.println("Nationality: " + nationality);
+ 
+}     
+
         
 
 
